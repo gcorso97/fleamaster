d4e61f2cc22b93765d560fe8b0f7f21d2839cbfd
gcorso97 <36161836+gcorso97@users.noreply.github.com>
2018-04-15 21:50:39 +0200

    enhance frontend login handling; preparation for real login handling

--- app/www/components/login-page.vue
+++ app/www/components/login-page.vue
@@ -1,23 +1,38 @@
 <template>
     <div id="login">
-        <div id="logoImage" class="centered"></div>
-        <div id="loginInput" class="centered">
-            <input class="centered" type="text" />
-            <input class="centered" type="text" />
-        </div>
-        <div id="loginLinks" class="centered">
-            <a href="#">Passwort zurücksetzen</a>
-            <a href="#">Registieren</a>
-        </div>
-        <button id="loginBtn" class="centered">Login</button>
+        <form @submit.prevent="login">
+            <div id="logoImage" class="centered"></div>
+            <div id="loginInput" class="centered">
+                <input class="centered" type="text" v-model="mail" />
+                <input class="centered" type="password" v-model="password" />
+            </div>
+            <div id="loginLinks" class="centered">
+                <a href="#">Passwort zurücksetzen</a>
+                <a href="#">Registieren</a>
+            </div>
+            <button id="loginBtn" class="centered">Login</button>
+        </form>
     </div>
 </template>
 
 <script>
 export default {
     data: function() {
-        return {};
+        return {
+            mail: '',
+            password: ''
+        };
     },
-    methods: {}
+    methods: {
+        login: function() {
+            var self = this;
+
+            self.$http.post(RESTURL + '/login', {mail: self.mail, password: self.password}).then(function(response) {
+                console.log(response);
+            }, function(response) {
+                console.error(response);
+            });
+        }
+    }
 };
 </script>
