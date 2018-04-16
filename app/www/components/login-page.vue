908d9bbd837fd9a6eafdd4b4e7b5d129e772d9da
Nast, Denise <Denise.Nast@dvag.com>
2018-04-16 11:13:07 +0200

    Login validation -> Login is only possible if the input is okay

--- app/www/components/login-page.vue
+++ app/www/components/login-page.vue
@@ -3,14 +3,14 @@
         <form @submit.prevent="login">
             <div id="logoImage" class="centered"></div>
             <div id="loginInput" class="centered">
-                <input class="centered" type="text" v-model="mail" />
-                <input class="centered" type="password" v-model="password" />
+                <input class="centered" type="text" v-model="mail" id="mail" v-on:input="validateInput"/>
+                <input class="centered" type="password" v-model="password" id="password" v-on:input="validateInput"/>
             </div>
             <div id="loginLinks" class="centered">
                 <a href="#">Passwort zurücksetzen</a>
                 <a href="#">Registieren</a>
             </div>
-            <button id="loginBtn" class="centered">Login</button>
+            <button id="loginBtn" class="centered" disabled>Login</button>
         </form>
     </div>
 </template>
@@ -20,10 +20,39 @@ export default {
     data: function() {
         return {
             mail: '',
-            password: ''
+            password: '',
+            regExpMail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+
+
         };
     },
     methods: {
+      validateInput: function () {
+        var self = this;
+        var mailOk = self.validEmail(self.mail);
+        var passwordOk = self.validPasswort(self.password);
+        if (mailOk && passwordOk) {
+            loginBtn.disabled = false;
+        } else {
+            loginBtn.disabled = true;
+        }
+      },
+
+      validEmail:function(email) {
+      var self = this;
+      return self.regExpMail.test(email);
+      },
+
+    validPasswort:function(password) {
+      var self = this;
+      var passwordLength = self.password.length;
+        if (passwordLength >= 6) {
+          return true;
+        } else {
+          return false;
+      }
+    },
+
         login: function() {
             var self = this;
 
