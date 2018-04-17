89400ff5568413b0f692633b7ddac38936d8a2da
Ashikaryu <xxryu97xx@gmail.com>
2018-04-17 09:28:33 +0200

    enhance user table sql

--- server/db/db_template.sql
+++ server/db/db_template.sql
@@ -3,6 +3,13 @@ CREATE DATABASE IF NOT EXISTS `fleamaster`;
 
 -- user table
 CREATE TABLE IF NOT EXISTS `user` (
-    `mail` VARCHAR(100) NOT NULL PRIMARY KEY,
+    'userid' smallint auto_increment primary key,
+    'firstname' varchar(30),
+    'lastname' varchar(30),
+    'country' varchar(50),
+    'city' varchar(50),
+    'zip' varchar(10),
+    'street' varchar(80),
+    `mail` VARCHAR(100) NOT NULL,
     `pwd_hash` VARCHAR(255) NOT NULL
 );
