28ae3fad02bdf420a0c71d286456a12a8243ce73
Ashikaryu <xxryu97xx@gmail.com>
2018-04-19 12:22:56 +0200

    Database extended

--- server/db/db_template.sql
+++ server/db/db_template.sql
@@ -6,10 +6,35 @@ CREATE TABLE IF NOT EXISTS `user` (
     'userid' smallint auto_increment primary key,
     'firstname' varchar(30),
     'lastname' varchar(30),
-    'country' varchar(50),
+    'gender' varchar(4),
     'city' varchar(50),
     'zip' varchar(10),
     'street' varchar(80),
     `mail` VARCHAR(100) NOT NULL,
     `pwd_hash` VARCHAR(255) NOT NULL
+);
+
+Create table 'shoppingCart'(
+  'userid' smallint,
+  'artikelid' smallint,
+  'totalprice' float,
+  foreign key userid references user(userid),
+  foreign key artikelid references artikel(artikelid),
+  primary key (userid,artikelid),
+);
+
+Create table 'artikel'(
+  'artikelid' smallint primary key,
+  'provider' smallint,
+  'bezeichnung' varchar(50),
+  'description' varchar(255),
+  'categoryid' smallint,
+  'preis' float,
+  foreign key categoryid references artikel(categoryid),
+  foreign key provider references user(userid)
+);
+
+CREATE table 'category'(
+  'categoryid' smallint primary key,
+  'categoryname' varchar(255)
 );
