b68b762148a88b808a201bc337c6d2238ef4daed
gcorso97 <36161836+gcorso97@users.noreply.github.com>
2018-05-05 23:11:49 +0200

    modify createEnv script to install dependencies directly

--- server/createEnv.sh
+++ server/createEnv.sh
@@ -3,10 +3,6 @@
 if [[ ! -e srv_config.json ]]; then
     cat srv_config.template.json > srv_config.json
 fi
-# create temporarly package.json to the top for travis-ci
-if [[ ! -e ../package.json ]]; then
-    cat package.json > ../package.json
-fi
 # setup mysql config
 config=`cat srv_config.json`;
 MYSQL_DATABASE="$(node -pe "JSON.parse(\`$config\`).DB_DBNAME")"
@@ -16,3 +12,6 @@ MYSQL_PASSWORD="$(node -pe "JSON.parse(\`$config\`).DB_PWD")"
 # database setup
 mysql -u ${MYSQL_USER} ${MYSQL_PASSWORD:+-p${MYSQL_PASSWORD}} --execute="CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}";
 mysql -u ${MYSQL_USER} ${MYSQL_DATABASE} ${MYSQL_PASSWORD:+-p${MYSQL_PASSWORD}} < modules/db/db_template.sql
+
+# install dependencies
+npm install
