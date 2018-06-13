5da7adc547bd087c59676abfafb2862cc103c624
gcorso97 <36161836+gcorso97@users.noreply.github.com>
2018-06-13 21:51:38 +0200

    add handling for products page to retrieve distance for each article

--- app/www/components/products-page.vue
+++ app/www/components/products-page.vue
@@ -26,9 +26,9 @@
                                 <span>{{ item.title}}</span>
                                 <span>{{ item.description}}</span>
                             </div>
-                            <div class="md-subhead location-icon-pin">
+                            <div class="md-subhead location-icon-pin" v-if="isBuyer && !buyhistory && item.distance">
                                 <md-icon>location_on</md-icon>
-                                <span>0km</span>
+                                <span>{{item.distance}}</span>
                             </div>
                             <span><b>{{ item.price}}€</b></span>
                         </md-list-item>
@@ -77,14 +77,47 @@
                 this.$router.push('addItem');
             },
             getItems: function () {
-                var self = this;
+                var self = this,
+                    articles = [],
+                    processedArticles = 0;
 
                 self.loading = true;
                 self.isBuyer = ((self.$route.query.isBuyer === 'true' || self.$route.query.isBuyer === true) ? true : false);
 
                 self.$http.get(RESTURL + ((self.isBuyer) ? ((self.buyhistory)? '/boughtarticles' : '/articles') : '/soldarticles'), {}).then(function (response) {
-                    self.loading = false;
-                    self.items = response.body.articles;
+                    // retrieve distance for each article
+                    if(response.body.articles && response.body.articles.length) {
+                        // get current position
+                        navigator.geolocation.getCurrentPosition(function(positionObj) {
+                            response.body.articles.forEach(function(articleObj) {
+                                self.$http.get(RESTURL + '/location', {
+                                    params: {
+                                        article: articleObj.id,
+                                        lat: positionObj.coords.latitude,
+                                        lng: positionObj.coords.longitude
+                                    }
+                                }).then(function(locationResponse) {
+                                    // attach the destination
+                                    articleObj.distance = locationResponse.body.distance;
+                                    articles.push(articleObj);
+                                    if(++processedArticles === response.body.articles.length) {
+                                        self.loading = false;
+                                        self.items = articles;
+                                    }
+                                }, function(error) {
+                                    // location could not be resolved
+                                    articles.push(articleObj);
+                                    if(++processedArticles === response.body.articles.length) {
+                                        self.loading = false;
+                                        self.items = articles;
+                                    }
+                                });
+                            });
+                        }, function(err) {
+                            self.loading = false;
+                            console.error(err); // TODO show dialog that location could not be resolved (permission denied?) + Cordova Plugin GeoLocation
+                        }, {enableHighAccuracy: true});
+                    }
                 }, function (error) {
                     console.error(error);
                     self.loading = false;
